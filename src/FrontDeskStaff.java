5ddfd07329ae6634f76418d0020cf9f3aed2475e
unknown <asarasensei@gmail.com>
2020-11-26 15:41:14 +0800

    cleaned code

--- src/FrontDeskStaff.java
+++ src/FrontDeskStaff.java
@@ -3,8 +3,6 @@ import java.util.List;
 
 public class FrontDeskStaff extends User {
     
-    ArrayList<String> order = new ArrayList<>();
-    List<SParcel> parcel = new ArrayList<SParcel>();
 
     public FrontDeskStaff(User a) {
         super(a.loginid,a.password,a.name,a.address,a.phone);
